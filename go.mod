a9947f452350346ba46507de20876f84d3a11c57
Yevhen Lebid <yevhen.lebid@loopme.com>
2020-04-08 15:50:50 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ealebed/gosed
+
+go 1.13
---
+++ gosed.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	info, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	if info.Mode()&os.ModeNamedPipe == 0 {
+		fmt.Println("The command is intended to work with pipes.")
+		fmt.Println("Usage: kubectl logs -f [pod_name] | ./gosed")
+		return
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+	for scanner.Scan() {
+		fmt.Println(strings.ReplaceAll(scanner.Text(), "u2028", "\n"))
+	}
+
+}
